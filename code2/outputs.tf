2c9b81ecd6366a040c54ece88fa59f1ce1fc7c7a
Михаил Дьячков <coldargentum@gmail.com>
2026-03-06 12:21:42 +0200

    Delete code2 directory

--- code2/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "instance_public_ip" {
-  description = "Public IP of the created EC2 instance."
-  value       = module.nginx_server.instance_public_ip
-}
--- code2/terraform.tfvars
+++
@@ -1,3 +0,0 @@
-vpc_id             = "vpc-0eeda6e433f8c9db1"
-subnet_id          = "subnet-017ce7178f926d6bb"
-list_of_open_ports = [22, 80]
